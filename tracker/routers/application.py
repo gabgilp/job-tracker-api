4f49ef5e872b9c0c4cfe2b14a725335109d34e40
gabgilp <gabriel@gabgilp.dev>
2025-05-08 16:44:12 +0200

    Create application finished

--- tracker/routers/application.py
+++ tracker/routers/application.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status, Depends, Header
 from sqlalchemy.ext.asyncio import AsyncSession
-from tracker.database import get_db
+from tracker.database import get_db, ApplicationTable
 from tracker.routers.utils import get_token, verify_user_id
 from tracker.auth.utils import verify_and_refresh_jwt
 from jose import JWTError
@@ -26,11 +26,25 @@ async def create_application(application: ApplicationIn,
     await verify_user_id(db, application.user_id, username)
     
 
-    print(f"Creating application for user {username} with data: {application}")
+    print(f"Creating application for user {username}")
+
+    new_application = ApplicationTable(
+        user_id=application.user_id,
+        company_name=application.company_name,
+        position_title=application.position_title,
+        status=application.status,
+        notes=application.notes,
+        date_applied=application.date_applied,
+        posting_url=application.posting_url
+    )
+
+    db.add(new_application)
+    await db.commit()
+    await db.refresh(new_application)
 
     return {
         "message": "Application created successfully",
-        "application": application
+        "application": new_application
     }
 
 
